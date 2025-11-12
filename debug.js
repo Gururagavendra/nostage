babda2a7ae70110df3c3b2d6e4bc7d2acafa501f
Test User <test@example.com>
2025-11-12 14:58:30 +0900

    Initial commit

---
+++ debug.js
@@ -0,0 +1,3 @@
+// This is a debug file - should not be committed
+console.log("DEBUG MODE ENABLED");
+console.log("Testing feature X");
---
+++ feature.py
@@ -0,0 +1,8 @@
+"""Main feature file."""
+
+def my_feature():
+    """This is the real feature code."""
+    return "Hello from my feature!"
+
+if __name__ == "__main__":
+    print(my_feature())
